9647747976f830a6fdb13358e5f05731122366c7
Shubham Singhal <shubh98singhal@gmail.com>
2020-09-01 14:57:44 +0530

    Updates

---
+++ precache-manifest.ec1856e9c7e98cbfd493485834c772cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596b78455d37a61fff46a40bc0c7f975",
+    "url": "/ReactNetflixWebsite/index.html"
+  },
+  {
+    "revision": "d3fc8e58c5b859bb7618",
+    "url": "/ReactNetflixWebsite/static/css/main.9795907d.chunk.css"
+  },
+  {
+    "revision": "f5212b02ab2d05c1aa57",
+    "url": "/ReactNetflixWebsite/static/js/2.3b235f22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactNetflixWebsite/static/js/2.3b235f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3fc8e58c5b859bb7618",
+    "url": "/ReactNetflixWebsite/static/js/main.307f5d42.chunk.js"
+  },
+  {
+    "revision": "2751e4cfe9d94e4fc0cb",
+    "url": "/ReactNetflixWebsite/static/js/runtime-main.e7e5162a.js"
+  }
+]);
